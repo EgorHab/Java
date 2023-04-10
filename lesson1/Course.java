c3679f6c58c70be5d3c0d559a73279cb872629b2
Egor Belogradov <125624162+EgorHab@users.noreply.github.com>
2023-04-10 21:30:31 +1000

    JavaCoreLesson1

---
+++ lesson1/Course.java
@@ -0,0 +1,21 @@
+package JavaCore.lesson1;
+
+public class Course {
+    private String name;
+    private int difficulty;
+    public Course (String name, int difficulty) {
+        this.name = name;
+        this.difficulty = difficulty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+
+
+}
