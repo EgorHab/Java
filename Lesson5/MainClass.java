8476d3fe83e1f88e65f44126bd9676e684f9f041
Egor Belogradov <125624162+EgorHab@users.noreply.github.com>
2023-04-23 21:43:49 +1000

    HW5

---
+++ Lesson5/MainClass.java
@@ -0,0 +1,10 @@
+package JavaCore.Lesson5;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        SaveData saveData = new SaveData();
+        saveData.writeFile();
+        saveData.readFile();
+    }
+}
